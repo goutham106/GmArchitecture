e78951c8301898c315fc893c89ece266e34583ae
Goutham <goutham.gm11@gmail.com>
2017-07-26 16:19:27 +0530

    SmsVerification Added

---
+++ app/src/main/java/com/gm/ui/smsverify/SmsVerifyActivity.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) 2017 Gowtham Parimelazhagan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.gm.ui.smsverify;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.gm.R;
+import com.gm.common.smsverify.OnSmsCatchListener;
+import com.gm.common.smsverify.SmsVerifyCatcher;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SmsVerifyActivity extends AppCompatActivity {
+
+    private SmsVerifyCatcher smsVerifyCatcher;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sms_verify);
+
+        //init views
+        final EditText etCode = (EditText) findViewById(R.id.et_code);
+        final Button btnVerify = (Button) findViewById(R.id.btn_verify);
+
+        //init SmsVerifyCatcher
+        smsVerifyCatcher = new SmsVerifyCatcher(this, new OnSmsCatchListener<String>() {
+            @Override
+            public void onSmsCatch(String message) {
+                String code = parseCode(message);//Parse verification code
+                etCode.setText(code);//set code in edit text
+                //then you can send verification code to server
+            }
+        });
+
+        //set phone number filter if needed
+        smsVerifyCatcher.setPhoneNumberFilter("777");
+        //smsVerifyCatcher.setFilter("regexp");
+
+        //button for sending verification code manual
+        btnVerify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //send verification code to server
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        smsVerifyCatcher.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        smsVerifyCatcher.onStop();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        smsVerifyCatcher.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    /**
+     * Parse verification code
+     *
+     * @param message sms message
+     * @return only four numbers from massage string
+     */
+    private String parseCode(String message) {
+        Pattern p = Pattern.compile("\\b\\d{4}\\b");
+        Matcher m = p.matcher(message);
+        String code = "";
+        while (m.find()) {
+            code = m.group(0);
+        }
+        return code;
+    }
+
+}
